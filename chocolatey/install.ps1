1c6d53fd0040ea266a4948cc37ac249c1192fc92
Florian JUDITH <florian.judith.b@gmail.com>
2023-04-24 07:21:41 -0400

    Added Chocolatey script

--- chocolatey/install.ps1
+++ chocolatey/install.ps1
@@ -11,6 +11,6 @@ If (Get-ChildItem -Path "$ScriptPath/packages.config")
 Else
 {
     Write-Verbose -Message "Using package configuration from remote Git repository."
-    Invoke-WebRequest -Uri https://raw.githubusercontent.com/fjudith/windows-devops-tools/main/chocolatey/packages.config -OutFile "$env:TEMP\packages.config"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/fjudith/windows-devops-tools/main/chocolatey/packages.config" -OutFile "$env:TEMP\packages.config"
     Invoke-Expression -Command "choco install -y $env:TEMP\packages.config"
 }
